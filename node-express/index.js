59eab714dad72d4598b5b81b6116039cfcd75032
harsh mishra <mishra.harshmishra727@gmail.com>
2022-04-21 00:23:53 +0530

    Express serve static files

--- node-express/index.js
+++ node-express/index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
      http = require('http');
+const morgan = require('Morgan');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,8 +12,12 @@ app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
 
 const server = http.createServer(app);
 
